757a76dc1a2fb13f0a1dc51ea9e9507de357608b
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-11 04:27:01 +0900

    arrange_zsh_history.py

---
+++ arrange_zsh_history.py
@@ -0,0 +1,23 @@
+import subprocess
+import re
+
+# zsh_historyの: 0123456789:0;hogehoge という奴をソートするつ
+
+contents = subprocess.check_output('cat .zsh_history', shell=True)
+data_list_b = contents.splitlines()
+s = r': [0-9]{10}:[0-9];'
+data_list = [repr(l).lstrip('b').strip('\'') for l in data_list_b]
+
+result_set = set([re.sub(s, '', d) for d in data_list])
+result = ''
+n = len(list(result_set))
+f = open('.zsh_history', 'w')
+for r in list(result_set):
+    if r != '':
+        r = r.replace("\\\\", "\\")
+        if r[0] == '\"':
+            r = r.strip('\"')
+        f.write(r+'\n')
+f.close()
+
+
