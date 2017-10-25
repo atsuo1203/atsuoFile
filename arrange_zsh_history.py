c2c082f50f3653139105f933704d10f0fd84088b
atsuo1203 <dondonkitemasu@gmail.com>
2017-10-26 01:31:00 +0900

    arrange_zsh_history

--- arrange_zsh_history.py
+++ arrange_zsh_history.py
@@ -9,12 +9,11 @@ s = r': [0-9]{10}:[0-9];'
 data_list = [repr(l).lstrip('b').strip('\'') for l in data_list_b]
 
 result_set = set([re.sub(s, '', d) for d in data_list])
-result = ''
-n = len(list(result_set))
 f = open('.zsh_history', 'w')
 for r in list(result_set):
     if r != '':
-        r = r.replace("\\\\", "\\")
+        p = re.compile(r'\\+')
+        r = p.sub(r'\\', r)
         if r[0] == '\"':
             r = r.strip('\"')
         f.write(r+'\n')
